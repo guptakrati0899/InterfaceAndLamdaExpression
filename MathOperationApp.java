30b6e60b17311e78a7ca5173294fa0762f39161d
KratiGupta <kratigupta8545@gmail.com>
2021-08-18 12:46:50 +0530

    Added welcome msg successfully

---
+++ MathOperationApp.java
@@ -0,0 +1,11 @@
+package mathop;
+
+
+
+public class MathOperationApp {
+	
+	public static void main(String[] args) {
+	System.out.println("Welcome to master branch of math operation app");
+	}
+
+}
