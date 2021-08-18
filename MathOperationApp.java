886522556b3e9333d437e5b4a654d64914e25f43
KratiGupta <kratigupta8545@gmail.com>
2021-08-18 12:53:49 +0530

    Added add,subtract,multiply,divide operation using Lambda

--- MathOperationApp.java
+++ MathOperationApp.java
@@ -1,11 +1,23 @@
 package mathop;
 
-
+interface IMathFunction {
+	int calculate(int a, int b);
+}
 
 public class MathOperationApp {
 	
 	public static void main(String[] args) {
-	System.out.println("Welcome to master branch of math operation app");
+		
+		IMathFunction add = (x, y) -> x + y;
+		IMathFunction subtract = (x, y) -> x - y;
+		IMathFunction multiply = (x, y) -> x * y;
+		IMathFunction divide = (x, y) -> x / y;
+
+
+		System.out.println("Addtion is " + add.calculate(5, 8));
+		System.out.println("Subtraction is " + subtract.calculate(8, 5));
+		System.out.println("Multiplication is " + multiply.calculate(5, 8));
+		System.out.println("Division is " + divide.calculate(8, 4));
 	}
 
 }
